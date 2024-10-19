934a0e23cd989aed18dffaef8b76d90092ecb73a
Heisenberg1719 <heisenberg@khazanapay.in>
2024-10-19 01:55:33 +0000

    space master

--- runner.py
+++ runner.py
@@ -3,10 +3,8 @@ from app import create_app
 from waitress import serve
 from app.utils.appconfig.config import ProductionConfig
 
-# Create the app instance with the desired configuration
 app = create_app(ProductionConfig)
 
-# Setup logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -18,8 +16,10 @@ if __name__ == '__main__':
     logger.info(f"Starting server on {host}:{port} with {'debug' if debug_mode else 'production'} mode with Using {threads} threads.")
 
     try:
+        
         if debug_mode:app.run(host=host, port=port, debug=True)
         else:serve(app, host=host, port=port, threads=threads)
+
     except KeyboardInterrupt:logger.info("Server shutdown initiated by user (KeyboardInterrupt).")
     except Exception as e:logger.error(f"An error occurred while starting the server: {e}", exc_info=True)
     finally:logger.info("Server has been stopped gracefully.")
